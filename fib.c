ec8a93eb2982e284d5e8db8c5309122dff728970
student <student@tuffix-vm.myguest.virtualbox.org>
2024-09-19 17:11:16 -0700

    Finished

--- fib.c
+++ fib.c
@@ -2,12 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <limits.h>
-unsigned long long int fib_wrapper[100000000];
+
 unsigned long long int fib_r_provider(unsigned long long int n)
 {
     if (n == 0 || n == 1) {return 0;}
     if (n == 2) {return 1;}
-    if (fib_wrapper[n] != 0) {return fib_wrapper[n];}
     else
     {
         if (fib_r_provider(n - 1) > ULLONG_MAX - fib_r_provider(n - 2))
@@ -15,17 +14,15 @@ unsigned long long int fib_r_provider(unsigned long long int n)
             printf("Error: overflow\nMax value: %llu\n", fib_r_provider(n - 1));
             exit(EXIT_FAILURE);
         }
-    	fib_wrapper[n] = fib_r_provider(n - 1) + fib_r_provider(n - 2);
-        return fib_wrapper[n];
+    	return fib_r_provider(n - 1) + fib_r_provider(n - 2);
     }
 }
 
 unsigned long long int fib_i_provider(unsigned long long int n)
 {
-    unsigned long long int first = 0, second = 1;
+    unsigned long long int first = 0, second = 1, sum = 0;
     if (n == 0) {return 0;}
     if (n == 1 || n == 2) {return 1;}
-    if (fib_wrapper[n] != 0) {return fib_wrapper[n];}
     else
     {
     	for (int i = 2; i < n; ++i)
@@ -35,17 +32,17 @@ unsigned long long int fib_i_provider(unsigned long long int n)
                 printf("Error: overflow\nMax value: %llu\n", second);
                 exit(EXIT_FAILURE);
             }
-    		fib_wrapper[n] = first + second;
+    		sum = first + second;
     		first = second;
-    		second = fib_wrapper[n];
+    		second = sum;
     	}
     }
-    return fib_wrapper[n];
+    return sum;
 }
 
 int main(int argc, char *argv[])
 {
-   unsigned long long int num = atoi(argv[1]);
+unsigned long long int num = atoi(argv[1]);
    if (!strcmp(argv[2], "i"))
    {
    	printf("%llu\n", fib_i_provider(num));
