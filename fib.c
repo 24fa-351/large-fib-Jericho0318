802d57d9b663a72874683ef893328b1baaa3586c
student <student@tuffix-vm.myguest.virtualbox.org>
2024-11-30 17:22:43 -0800

    Modified

--- fib.c
+++ fib.c
@@ -2,35 +2,36 @@
 #include <stdlib.h>
 #include <string.h>
 #include <limits.h>
+#include <stdint.h>
 
-unsigned long long int fib_r_provider(unsigned long long int n)
+uint64_t fib_r(uint64_t num)
 {
-    if (n == 0 || n == 1) {return 0;}
-    if (n == 2) {return 1;}
+    if (num == 0 || num == 1) {return 0;}
+    if (num == 2) {return 1;}
     else
     {
-        if (fib_r_provider(n - 1) > ULLONG_MAX - fib_r_provider(n - 2))
+        if (fib_r(num - 1) > ULLONG_MAX - fib_r(num - 2))
         {
-            printf("Error: overflow\nMax value: %llu\n", fib_r_provider(n - 1));
-            exit(EXIT_FAILURE);
+            printf("Error: overflow\nMax value: %lu\n", fib_r(num - 1));
+            exit(1);
         }
-    	return fib_r_provider(n - 1) + fib_r_provider(n - 2);
+    	return fib_r(num - 1) + fib_r(num - 2);
     }
 }
 
-unsigned long long int fib_i_provider(unsigned long long int n)
+uint64_t fib_i(uint64_t num)
 {
-    unsigned long long int first = 0, second = 1, sum = 0;
-    if (n == 0) {return 0;}
-    if (n == 1 || n == 2) {return 1;}
+    uint64_t first = 0, second = 1, sum = 0;
+    if (num == 0) {return 0;}
+    if (num == 1 || num == 2) {return 1;}
     else
     {
-    	for (int i = 2; i < n; ++i)
+    	for (int i = 2; i < num; ++i)
     	{
     		if (second > ULLONG_MAX - first)
             {
-                printf("Error: overflow\nMax value: %llu\n", second);
-                exit(EXIT_FAILURE);
+                printf("Error: overflow\nMax value: %lu\n", second);
+                exit(1);
             }
     		sum = first + second;
     		first = second;
@@ -42,12 +43,20 @@ unsigned long long int fib_i_provider(unsigned long long int n)
 
 int main(int argc, char *argv[])
 {
-unsigned long long int num = atoi(argv[1]);
-   if (!strcmp(argv[2], "i"))
-   {
-   	printf("%llu\n", fib_i_provider(num));
-   } else if (!strcmp(argv[2], "r")) {
-        printf("%llu\n", fib_r_provider(num));
-   }
-   return 0;
+    if (argc != 3) {
+        perror("Provide 3 arguments: ./<filename> <num> <i or r>\n");
+        exit(1);
+    }
+
+    uint64_t get_num = atoi(argv[1]);
+
+    if (!strcmp(argv[2], "i")) {
+        printf("%lu\n", fib_i(get_num));
+    } else if (!strcmp(argv[2], "r")) {
+        printf("%lu\n", fib_r(get_num));
+    } else {
+        perror("Choose between i or r in argv[2]\n");
+        exit(1);
+    }
+    return 0;
 }
